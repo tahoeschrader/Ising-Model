cd29604302611f0f54563c02a30cb72af99eb0e4
Tahoe Schrader <tahoeschrader@gmail.com>
2017-04-23 16:05:49 -0400

    Added plotting capabilities
    After plotting, bugs discovered in calculate of specific heat. Need to
    find errors. Code skeleton works completely, however. So... that's good
    news!

--- solveIsing.py
+++ solveIsing.py
@@ -19,6 +19,7 @@ from functions import InitializeIsingModel, MeasureMagnetization
 from functions import SpinFlip, MetropolisAlgorithm, EnergyExponential
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
 
 # ------------------------------------------------------------------------------
 
@@ -27,10 +28,9 @@ import matplotlib.pyplot as plt
 ################################################################################
 
 J = 1.5                     # nearest neighbor interaction strength
-B = 0.0                     # no external magnetic field
 kB = 1                      # Boltzmann constant is 1 for this code
 
-lattice_sizes = [3, 5]
+lattice_sizes = [5, 10]
 #lattice_sizes = [5, 10, 20, 30, 40, 50, 75, 100, 200, 500]
 
 runs = 50                   # number of runs
@@ -44,16 +44,25 @@ Nm = 50                     # number of times times we calculate energy before
 ### Evolve the Model as a function of temperature for multiple lattice sizes
 ################################################################################
 
-# Loop across all lattice sizes
-for n in range(0, len(lattice_sizes)) :
-    # Create empty vectors to save energy values needed for specific heat
-    energies = []
-    energies2 = []
-    dE2s = []
-    c = []
+# Initialize a list to store the time it takes to run the code
+runtime = []
 
-    # Create empty vector for magnetizations
-    magnetizations = []
+# Initialize a list to save c_max values
+c_maxs = []
+
+# Loop across all lattice sizes
+for n in range(len(lattice_sizes)) :
+    # Save the time it starts
+    start_time = time.time()
+    print "Running the ", lattice_sizes[n], "x", lattice_sizes[n], " lattice"
+
+    # Create empty vectors to specific heat per spin, specific heat
+    c_per_N = []
+    cs = []
+
+    # Create a list to save magnetizations if n = 50
+    if lattice_sizes[n] == 5 :
+        magnetizations = []
 
     # Initialize the lattice and grab its energy
     lattice, energy = InitializeIsingModel(J, lattice_sizes[n])
@@ -83,24 +92,74 @@ for n in range(0, len(lattice_sizes)) :
         energy = np.sum(energy_runs) / Nm
         energy2 = np.sum(energy_runs**2) / Nm
         dE2 = energy2 - (energy**2)
-
-        # Calculate various energies and specific heat
-        energies.append(energy)
-        energies2.append(energy2)
-        dE2s.append(dE2)
-
-        # Need to append specific heat value, but not sure what to do about the
-        # division by zero, so I just replot the same value as the one before it
-        # and ignore it when doing data analysis.
         if T[temp] != 0 :
-            c.append(dE2/(kB*(T[temp]**2)))
+            c = dE2 / (kB * (T[temp]**2))
         elif T[temp] == 0 :
-            c.append(dE2/(kB*(T[temp - 1]**2)))
+            c = dE2 / (kB * (T[temp - 1]**2))
 
-        # Calculate magnetizations
-        magnetization = MeasureMagnetization(lattice)
-        magnetizations.append(magnetization)
+        # Save specific heats
+        cs.append(c)
+        c_per_N.append(c / (lattice_sizes[n]**2))
 
-# Finish counting time
-end = time.time()
-print "This code finished after ", (end-start), " seconds"
+        # Save magnetizations if n = 50
+        if lattice_sizes[n] == 5 :
+            magnetization = MeasureMagnetization(lattice)
+            magnetizations.append(magnetization)
+
+    # Save the time it ended
+    end_time = time.time()
+    time_elapsed = end_time - start_time
+    runtime.append(time_elapsed)
+    print "This lattice has cooled down."
+
+    # Now that the lattice has cooled down and the data has been stored, I must
+    # find the maximum c value and store it in a list
+    c_maxs.append(max(c_per_N))
+
+    # I want to show a couple plots of c(T) for various n
+    if lattice_sizes[n] == 5 or lattice_sizes[n] == 50 or lattice_sizes[n] == 500 :
+        fig = plt.subplot()
+        plt.title('Specific heat for a $%i\\times%i$ lattice' %(lattice_sizes[n],lattice_sizes[n]), fontsize = 20 )
+        plt.plot(T, cs, color = 'dodgerblue', linestyle = 'solid', linewidth = 3, label = '$C = \\frac{(\\Delta E^2)}{kB\; T^2}$')
+        plt.ylabel('specific heat, $C$', fontsize = 15)
+        plt.xlabel('temperature, $T$', fontsize = 15)
+        plt.legend()
+        plt.grid()
+        plt.show()
+
+################################################################################
+### Plot the data
+### I want to plot the following relations:
+###         1) For n = 50: M vs. T and find Tc (critical temperature)
+###         2) c_max/N vs n and fit log(n) to the plot
+###         3) Time relation of how long it takes to code this
+################################################################################
+
+# Starting with plot one (still needs to find Tc)
+fig = plt.subplot()
+plt.title('Magnetization for a $50\\times 50$ lattice', fontsize = 20 )
+plt.plot(T, magnetizations, color = 'dodgerblue', linestyle = 'solid', linewidth = 3, label = '$M = N \langle s \\rangle = \sum_i\; \\rho_i$')
+plt.ylabel('magnetization, $M$', fontsize = 15)
+plt.xlabel('temperature, $T$', fontsize = 15)
+plt.legend()
+plt.grid()
+plt.show()
+
+# Plot two (still needs to fit log(n))
+fig = plt.subplot()
+plt.title('Verification of the finite-size scaling relation', fontsize = 20 )
+plt.plot(lattice_sizes, c_maxs, color = 'dodgerblue', linestyle = 'solid', linewidth = 3, label = 'data')
+plt.ylabel('specific heat per spin, $C/N$', fontsize = 15)
+plt.xlabel('$1D$ lattice dimension, $n$', fontsize = 15)
+plt.legend()
+plt.grid()
+plt.show()
+
+# Plot three
+fig = plt.subplot()
+plt.title('Code runtime', fontsize = 20 )
+plt.plot(lattice_sizes, runtime, color = 'dodgerblue', linestyle = 'solid', linewidth = 3)
+plt.ylabel('time, $s$', fontsize = 15)
+plt.xlabel('$1D$ lattice dimension, $n$', fontsize = 15)
+plt.grid()
+plt.show()
